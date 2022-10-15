4d85004116169caf57698da4586dd58f1f1a3288
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-10-15 13:02:45 +0200

    Feat (Calculate numbers - Addition) : add productive code

--- src/main/java/org/example/cucumberdemo/Calculator.java
+++ src/main/java/org/example/cucumberdemo/Calculator.java
@@ -2,7 +2,7 @@ package org.example.cucumberdemo;
 
 public class Calculator {
 
-    public int add(int number1, int number2) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public int add(Integer number1, int number2) {
+        return number1 + number2;
     }
 }
